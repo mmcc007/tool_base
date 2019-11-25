319556b75976c129d20875e5b9ff09556c09ee49
Maurice McCabe <mmcc007@gmail.com>
2019-11-24 23:55:03 -0800

    Fixed test

--- test/pub_version_test.dart
+++ test/pub_version_test.dart
@@ -5,14 +5,19 @@
  * found in the LICENSE file.
  */
 
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:file/file.dart';
 import 'package:file/memory.dart';
 import 'package:http/http.dart' as http;
-import 'package:mockito/mockito.dart';
 import 'package:pub_semver/pub_semver.dart' as v;
 import 'package:test/test.dart';
-import 'package:tool_base/tool_base.dart';
+import 'package:tool_base/src/base/config.dart';
+import 'package:tool_base/src/base/file_system.dart';
+import 'package:tool_base/src/base/net.dart';
 
 import 'src/context.dart';
 
@@ -79,30 +84,52 @@ void main() {
   });
 
   group('pub version', () {
+    MemoryFileSystem fs;
+
+    setUp(() {
+      fs = MemoryFileSystem();
+    });
+
     testUsingContext('get version remotely', () async {
       final PubVersion pubVersion = PubVersion('sylph', 'settings');
       final String version = await pubVersion.getLatestVersion();
-      expect(version, 'version');
+      final String savedVersion =
+          jsonDecode(fs.file('settings').readAsStringSync())['latestVersion'];
+      print(fs.file('settings').readAsStringSync());
+      expect(version, savedVersion);
+    }, overrides: <Type, Generator>{
+      FileSystem: () => fs,
+      HttpClientFactory: () =>
+          () => MockHttpClient(200, result: jsonEncode({
+            "scorecard": {
+              "packageName": "sylph",
+              "packageVersion": "0.7.0+2",
+              "updated": "2019-11-21T22:31:32.194619Z",
+              "packageVersionCreated": "2019-11-20T18:17:13.527154Z",
+            }
+          }
+          )),
     });
   });
 }
 
-/// The version of a published package.
+/// A persistent version of a published package.
 ///
 /// Retrieves from a settings file or, if absent, from a pub server.
 /// Stores in a settings file.
 ///
 class PubVersion {
-  PubVersion(this.packageName, this.settings, [this._client, this._url]);
+  PubVersion(this.packageName, this.settings, [this._url]);
 
   final String packageName;
   final String settings;
   Uri _url;
-  http.Client _client;
+
+//  http.Client _client;
   File _file;
   Config _config;
 
-  http.Client get client => _client ??= http.Client();
+//  http.Client get client => _client ??= http.Client();
 
   File get file => _file ??= fs.file(settings);
 
@@ -115,7 +142,7 @@ class PubVersion {
         scheme: 'https',
         host: kPubDevHost,
         path: '$kPubDevBasePath/$packageName/metrics',
-      query: 'pretty',
+        query: 'pretty',
       );
 
   Future<String> getLatestVersion() async =>
@@ -124,7 +151,7 @@ class PubVersion {
 //  void setVersion(String version) => config.setValue(kVersion, version);
 
   Future<String> getVersionDate() async =>
-      await _getVar(kVersionDate, 'packageCreated');
+      await _getVar(kVersionDate, 'updated');
 
 //  void setVersionDate(String versionDate) =>
 //      config.setValue(kVersionDate, versionDate);
@@ -132,8 +159,10 @@ class PubVersion {
   Future<String> _getVar(String varName, String metric) async {
     String varValue = config.getValue(varName);
     if (varValue == null) {
-      final http.Response response = await client.get(url);
-      final Map<String, dynamic> metrics = jsonDecode(response.body);
+      final List<int> charCodes = await fetchUrl(url);
+      print(charCodes);
+      final Map<String, dynamic> metrics =
+          jsonDecode(String.fromCharCodes(charCodes));
       varValue = metrics['scorecard'][metric];
       config.setValue(varName, varValue);
     }
@@ -141,4 +170,76 @@ class PubVersion {
   }
 }
 
-class MockFileSystem extends Mock implements FileSystem {}
+
+class MockHttpClient implements HttpClient {
+  MockHttpClient(this.statusCode, {this.result});
+
+  final int statusCode;
+  final String result;
+
+  @override
+  Future<HttpClientRequest> getUrl(Uri url) async {
+    return MockHttpClientRequest(statusCode, result: result);
+  }
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    throw 'io.HttpClient - $invocation';
+  }
+}
+
+class MockHttpClientRequest implements HttpClientRequest {
+  MockHttpClientRequest(this.statusCode, {this.result});
+
+  final int statusCode;
+  final String result;
+
+  @override
+  Future<HttpClientResponse> close() async {
+    return MockHttpClientResponse(statusCode, result: result);
+  }
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    throw 'io.HttpClientRequest - $invocation';
+  }
+}
+
+class MockHttpClientResponse implements HttpClientResponse {
+  MockHttpClientResponse(this.statusCode, {this.result});
+
+  @override
+  final int statusCode;
+
+  final String result;
+
+  @override
+  String get reasonPhrase => '<reason phrase>';
+
+  @override
+  HttpClientResponseCompressionState get compressionState {
+    return HttpClientResponseCompressionState.decompressed;
+  }
+
+  @override
+  StreamSubscription<Uint8List> listen(
+      void onData(Uint8List event), {
+        Function onError,
+        void onDone(),
+        bool cancelOnError,
+      }) {
+    return Stream<Uint8List>.fromIterable(<Uint8List>[Uint8List.fromList(result.codeUnits)])
+        .listen(onData, onError: onError, onDone: onDone, cancelOnError: cancelOnError);
+  }
+
+  @override
+  Future<dynamic> forEach(void Function(Uint8List element) action) {
+    action(Uint8List.fromList(result.codeUnits));
+    return Future<void>.value();
+  }
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    throw 'io.HttpClientResponse - $invocation';
+  }
+}
